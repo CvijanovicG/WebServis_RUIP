b392a483d40974dd89bf7cc529063495014817d4
CvijanovicG <cvijanovicg@gmail.com>
2022-04-07 12:49:11 +0200

    Branch_Services

---
+++ WebServis_RUIP/Services/IWS_KorisnickiPodaci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using WebServis_RUIP.Models;
+
+namespace WebServis_RUIP.Services
+{
+    [ServiceContract]
+    public interface IWS_KorisnickiPodaci
+    {
+        [OperationContract]
+        public List<KorisnickiPodaciModel> PronadjiKorisnika(PodaciZaPretraguModel podaciZaPretragu);
+    }
+}
---
+++ WebServis_RUIP/Services/WS_KorisnickiPodaci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebServis_RUIP.Models;
+
+namespace WebServis_RUIP.Services
+{
+    public class WS_KorisnickiPodaci : IWS_KorisnickiPodaci
+    {
+        private RepozitorijGlavni _db;
+
+        public WS_KorisnickiPodaci(
+            RepozitorijGlavni repozitorij
+            )
+        {
+            _db = repozitorij;
+        }
+
+        public List<KorisnickiPodaciModel> PronadjiKorisnika(PodaciZaPretraguModel podaciZaPretragu)
+        {
+            List<KorisnickiPodaciModel> rezultat;
+
+            try
+            {
+                rezultat = _db.PronadjiKorisnika(podaciZaPretragu);
+
+                return rezultat;
+            }
+            catch (Exception ex)
+            {
+                _db.LogujGresku(ex);
+                throw new Exception($"Greška prilikom izvršavanja servisa! Opis greške: {ex.Message}");
+            }
+
+        }
+    }
+}
