f0c1dd612b52fce43b5c04d78a614ecac5d1120b
CvijanovicG <cvijanovicg@gmail.com>
2022-04-07 13:52:26 +0200

    Branch_Startup

--- WebServis_RUIP/Startup.cs
+++ WebServis_RUIP/Startup.cs
@@ -2,19 +2,25 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using SoapCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
+using WebServis_RUIP.Repozitorij;
+using WebServis_RUIP.Services;
 
 namespace WebServis_RUIP
 {
     public class Startup
     {
+        private IConfiguration _config { get; }
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -22,6 +28,13 @@ namespace WebServis_RUIP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSoapCore();
+            services.AddSingleton<RepozitorijGlavni>();
+            services.TryAddSingleton<IWS_KorisnickiPodaci, WS_KorisnickiPodaci>();
+            // Moze se dodati ova linija, u slucaju exception-a da klijent dobije poruku
+            services.AddSoapExceptionTransformer((ex) => ex.Message);
+
+            services.AddRazorPages();
             services.AddControllersWithViews();
         }
 
@@ -39,7 +52,7 @@ namespace WebServis_RUIP
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseSoapEndpoint<IWS_KorisnickiPodaci>(_config.GetValue<string>("EndpointUrl"), new BasicHttpBinding(), SoapSerializer.XmlSerializer);
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
