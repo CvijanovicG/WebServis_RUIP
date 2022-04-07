cb82ee5624a1e9dfb3153bc29872374a264ec1c8
CvijanovicG <cvijanovicg@gmail.com>
2022-04-07 10:53:41 +0200

    Branch_InputClass with NuGet

---
+++ WebServis_RUIP/Models/PodaciZaPretraguModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServis_RUIP.Models
+{
+    public class PodaciZaPretraguModel
+    {
+        public string JMBGOsiguranika { get; set; }
+        public string JIBPoslodavca { get; set; }
+        public DateTime Datum { get; set; }
+       
+    }
+}
---
+++ WebServis_RUIP/PodaciZaPretraguModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServis_RUIP
+{
+    public class PodaciZaPretraguModel
+    {
+    }
+}
