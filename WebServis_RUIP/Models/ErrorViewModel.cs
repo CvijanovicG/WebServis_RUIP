081677d3b0252666a652ae76297ea631f844447c
CvijanovicG <cvijanovicg@gmail.com>
2022-04-07 10:32:04 +0200

    Add project files.

---
+++ WebServis_RUIP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebServis_RUIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
